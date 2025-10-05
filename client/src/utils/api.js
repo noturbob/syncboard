e26031e4fd205344052b23b10457fb308bb7ce83
noturbob <bobbyanthenrao@gmail.com>
2025-10-05 13:19:34 +0530

    Fix: Use production API URL for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  // This line now dynamically sets the backend URL
+  baseURL: process.env.REACT_APP_API_URL ? `${process.env.REACT_APP_API_URL}/api` : 'http://localhost:4000/api',
   headers: {
     'Content-Type': 'application/json',
   },
